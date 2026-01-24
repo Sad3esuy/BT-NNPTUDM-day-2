0f9d5abb4fa93d7a09570493bd35c08524826583
Sad3esuy <nguyenquyngoc2004@gmail.com>
2026-01-24 17:16:22 +0700

    add btap day2

---
+++ README.md
@@ -0,0 +1,3 @@
+## Nguyễn Quý Ngọc  
+## 2280602095   
+## Bài tập cá nhân buổi 2 (24/01)
---
+++ main.js
@@ -0,0 +1,127 @@
+LoadData();
+async function LoadData() {
+    //async await 
+    //HTTP Request GET, GET1, PUT, POST, DELETE
+    try {
+        let res = await fetch('http://localhost:3000/posts');
+        let posts = await res.json();
+        let body = document.getElementById('post-body')
+        body.innerHTML = "";
+        for (const post of posts) {
+            body.innerHTML += convertDataToHTML(post);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+function convertDataToHTML(post) {
+    // Kiểm tra nếu bài viết đã xóa 
+    const isDisabled = post.isDeleted ? "disabled" : "";
+    const styleBlur = post.isDeleted ? "style='opacity: 0.5; background-color: #f2f2f2; font-style: italic; font-weight: bold; text-decoration: line-through;'" : "";
+
+    return `
+    <tr ${styleBlur}>
+        <td>${post.id}</td>
+        <td>${post.title}</td>
+        <td>${post.views}</td>
+        <td>
+            <input type='button' 
+                   value='${post.isDeleted ? "Deleted" : "Delete"}' 
+                   onclick='Delete("${post.id}")' 
+                   ${isDisabled} />
+        </td>
+    </tr>`;
+}
+
+//THÊM MỚI HOẶC CẬP NHẬT
+async function saveData() {
+    // Lấy dữ liệu từ input
+    let id = document.getElementById("id_txt").value.trim();
+    let title = document.getElementById("title_txt").value;
+    let view = document.getElementById('views_txt').value; 
+    
+    let checkUpdate = false;
+    // Kiểm tra xem ID có tồn tại để Update hay không
+    if(id){
+        let resGET = await fetch('http://localhost:3000/posts/' + id);      
+        if (resGET.ok ) {
+            let data = await resGET.json();
+            // Chỉ cho sửa nếu chưa bị xóa (isDeleted = false)
+            if(!data.isDeleted){
+                checkUpdate = true;
+            } 
+            else {
+                console.log("đã xoá => thêm mới");
+            }
+        }  
+    }
+
+
+    if (checkUpdate) {
+        let resPUT = await fetch('http://localhost:3000/posts/' + id, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title: title,
+                views: view,
+                isDeleted: false
+            })
+        });
+        if (resPUT.ok) console.log("Cập nhật thành công id: " + id);
+    } else {
+        // Lấy danh sách để tìm Max ID
+        let res = await fetch('http://localhost:3000/posts');
+        let posts = await res.json();
+        
+        let maxId = 0;
+        posts.forEach(post => {
+            let currentId = parseInt(post.id);
+            if (!isNaN(currentId) && currentId > maxId) {
+                maxId = currentId;
+            }
+        });
+        //Tạo ID mới (tăng lên 1 và chuyển thành String)
+        let newId = (maxId + 1).toString();
+        let resPOST = await fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: newId,
+                title: title,
+                views: view,
+                isDeleted: false
+            })
+        });
+        if (resPOST.ok) console.log("Thêm mới thành công với ID: " + newId);
+    }
+    return false;
+}
+// async function Delete(id) {
+//     let res = await fetch('http://localhost:3000/posts/' + id, {
+//         method: "delete"
+//     });
+//     if (res.ok) {
+//         console.log("xoa thanh cong");
+//         LoadData();
+//     }
+// }
+
+//xoá mềm không xoá hẳn bằng cách thêm trường isDeleted: true
+async function Delete(id) {
+    //Dùng PATCH để chỉ cập nhật trường isDeleted mà không mất dữ liệu khác
+    let res = await fetch('http://localhost:3000/posts/' + id, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                isDeleted: true
+            })
+    });
+    if (res.ok) {
+        console.log("xoa thanh cong");
+        LoadData();
+    }
+}
